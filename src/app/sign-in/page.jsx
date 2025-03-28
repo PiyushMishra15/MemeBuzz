57bab6cbb6ff98e816b48f459db5efc30a736be1
PiyushMishra15 <piyushmishra2915@gmail.com>
2025-03-28 22:27:00 +0530

    create login and sign up authentication with otp

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -1,18 +1,17 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import CustomSidebar from '@/components/SideBar';
-import toast from 'react-hot-toast';
-import Image from 'next/image';
-import { useIsMobile } from '@/hooks/use-mobile';
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import Image from "next/image";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { Eye, EyeOff } from "lucide-react"; // Icons for show/hide
 
 function SignInPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const router = useRouter();
   const isMobile = useIsMobile();
   const [showPassword, setShowPassword] = useState(false);
@@ -20,42 +19,52 @@ function SignInPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signin`, {
-      email: email.toLowerCase(),
-      password,
-    }).then((response) => {
-      toast.success('User signed in successfully');
-      console.log('User signed in successfully:', response.data);
-      // Save the token in localStorage or cookies
-      localStorage.setItem('token', response.data.data.token);
-      localStorage.setItem('user', (response.data.data.user._id));
-      router.push('/');
-
-    }).catch((error) => {
-      console.log('Error signing in user:', error);
-      toast.error(error?.response?.data?.error?.explanation || error?.response?.data?.error || 'An error occurred');
-
-    });
+    await axios
+      .post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signin`, {
+        email: email.toLowerCase(),
+        password,
+      })
+      .then((response) => {
+        toast.success("User signed in successfully");
+        console.log("User signed in successfully:", response.data);
+        // Save the token in localStorage or cookies
+        localStorage.setItem("token", response.data.data.token);
+        localStorage.setItem("user", response.data.data.user._id);
+        router.push("/");
+      })
+      .catch((error) => {
+        console.log("Error signing in user:", error);
+        toast.error(
+          error?.response?.data?.error?.explanation ||
+            error?.response?.data?.error ||
+            "An error occurred"
+        );
+      });
 
     // Redirect to the home page or another protected page
-
-
   };
 
   return (
-    <>    {isMobile && <header className="fixed inset-0  w-full bg-black text-white text-center py-4">
-      <div className="flex justify-between mx-2 md:mx-4 lg:mx-16">
-
-        <Image src="/exe.png" alt="logo" width={25} height={25} />
-        <h1 className="text-2xl mx-auto text-center justify-center  font-bold">MemeWebsite</h1>
-        <Image src="/exe.png" alt="logo" width={25} height={25} />
-      </div>
-    </header>}
+    <>
+      {" "}
+      {isMobile && (
+        <header className="fixed inset-0  w-full bg-black text-white text-center py-4">
+          <div className="flex justify-between mx-2 md:mx-4 lg:mx-16">
+            <Image src="/exe.png" alt="logo" width={25} height={25} />
+            <h1 className="text-2xl mx-auto text-center justify-center  font-bold">
+              MemeWebsite
+            </h1>
+            <Image src="/exe.png" alt="logo" width={25} height={25} />
+          </div>
+        </header>
+      )}
       <div className="flex">
         {/* <CustomSidebar /> */}
         <div className="flex-1 flex items-center justify-center min-h-screen bg-black">
           <div className="bg-white bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg w-full max-w-md">
-            <h1 className="text-2xl font-bold mb-4 text-center text-white">Sign In</h1>
+            <h1 className="text-2xl font-bold mb-4 text-center text-white">
+              Sign In
+            </h1>
             {error && <p className="text-red-500">{error}</p>}
             <form onSubmit={handleSubmit}>
               <div className="mb-4">
@@ -69,7 +78,7 @@ function SignInPage() {
               </div>
               <div className="mb-4">
                 <label className="block text-gray-300">Password</label>
-                <div className='flex gap-1'>
+                <div className="flex gap-1">
                   <input
                     type={showPassword ? "text" : "password"}
                     value={password}
@@ -83,7 +92,6 @@ function SignInPage() {
                   >
                     {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                   </button>
-
                 </div>
               </div>
               <button
@@ -97,11 +105,17 @@ function SignInPage() {
             <hr className="my-4 border-gray-300" />
             <div className="text-center mt-4 flex items-center justify-center">
               <p className="text-gray-300">Don't have an account?</p>
-              <a href="/sign-up" className="text-purple-500 hover:text-purple-300">Sign Up</a>
+              <a
+                href="/sign-up"
+                className="text-purple-500 hover:text-purple-300"
+              >
+                Sign Up
+              </a>
             </div>
           </div>
-        </div >
-      </div ></>
+        </div>
+      </div>
+    </>
   );
 }
 
